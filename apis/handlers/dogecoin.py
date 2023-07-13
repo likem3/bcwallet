99056f257b62773ac35b4521dc4997078d3f542b
likem3 <islike1221@gmail.com>
2023-07-13 20:12:39 +0800

    handle dogecoin and litecoin

--- apis/handlers/dogecoin.py
+++ apis/handlers/dogecoin.py
@@ -3,11 +3,60 @@ import requests
 from decimal import Decimal
 from apis.handlers import BaseHandler
 
+from django.conf import settings as app_settings
+import requests
+from decimal import Decimal
+from apis.handlers import BaseHandler
+
+
 class DOGEHandler(BaseHandler):
     def __init__(self):
         super().__init__()
-        self.url = app_settings.BLOCKCHAIN_DOGE_ADDR
-        self.url_test = app_settings.BLOCKCHAIN_DOGE_ADDR_TEST
+        self.url = app_settings.GETBLOCK_DOGECOIN_BLOCKBOOK_ADDR
+        self.url_test = app_settings.GETBLOCK_DOGECOIN_BLOCKBOOK_ADDR_TEST
+
+    def parsing_balance(self, response):
+        try:
+            resp_json = response.json()
+            balance_sts = Decimal(resp_json['balance'])
+            return  balance_sts / 100000000
+
+        except Exception as e:
+            print(str(e))
+            return
+
+    def get_balance(self, address):
+        url = f"{self.url}v2/address/{address}"
+        params = {
+            'page': 1,
+            'pageSize': 1,
+            'details': 'basic',
+            'secondary': 'ltc'
+        }
+
+        response = requests.get(url, params=params)
+
+        return self.parsing_balance(response)
+
+    def get_balance_test(self, address):
+        url = f"{self.url_test}v2/address/{address}"
+        params = {
+            'page': 1,
+            'pageSize': 1,
+            'details': 'basic',
+            'secondary': 'ltc'
+        }
+
+        response = requests.get(url, params=params)
+
+        return self.parsing_balance(response)
+
+
+class DOGEHandlerBlockChair(BaseHandler):
+    def __init__(self):
+        super().__init__()
+        self.url = app_settings.BLOCKCHAIR_DOGECOIN_ADDR
+        self.url_test = app_settings.BLOCKCHAIR_DOGECOIN_ADDR_TEST
 
     def parsing_balance(self, address, response):
         try:
