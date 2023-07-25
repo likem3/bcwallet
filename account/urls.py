a9ab65642f9729a72f171094a87f678c2c049581
likem3 <islike1221@gmail.com>
2023-07-25 09:54:21 +0800

    handle multiple lookup fields detail

--- account/urls.py
+++ account/urls.py
@@ -1,8 +1,8 @@
 from django.urls import path
 
 from account.views import (
-    UserDetailSuspendUserView,
     UserDetailSuspendView,
+    UserDetailMerchantView,
     UserListCreateView,
     WalletDetailView,
     WalletListByUserIDView,
@@ -17,12 +17,11 @@ urlpatterns = [
         name="user-detail-suspend-by-id",
     ),
     path(
-        "accounts/user/<int:user_id>/",
-        UserDetailSuspendUserView.as_view(),
-        name="user-detail-suspend-by-user_id",
+        "accounts/<int:merchant_code>/<int:user_id>/",
+        UserDetailMerchantView.as_view(),
+        name="user-detail-suspend-by-merchant_code-user_id",
     ),
     path("wallets/", WalletListCreateView.as_view(), name="wallets-list-create"),
-    # path("wallets/create/", WalletCreateView.as_view(), name="wallets-create"),
     path("wallets/<int:pk>/", WalletDetailView.as_view(), name="wallets-detail"),
     path(
         "wallets/user/<int:user_id>/",
--- account/views.py
+++ account/views.py
@@ -8,13 +8,14 @@ from account.models import Account, Wallet
 from account.schemas import create_wallet_schema, get_account_list_schema
 from account.serializers import AccountSerializer, CreateAccountSerializer, WalletSerializer
 from utils.paginations import SizePagePagination
+from utils.mixins import DetailMultipleFieldLookupMixin
 
 
 class UserListCreateView(generics.ListCreateAPIView):
     # permission_classes = [IsAuthenticated]
     queryset = Account.objects.filter(status="active")
     filter_backends = [filters.SearchFilter, filters.OrderingFilter]
-    search_fields = ["user_id", "email", "username"]
+    search_fields = ["merchant__code", "user_id", "email", "username"]
     ordering_fields = ["id", "-id", "created_at", "-created_at", "user_id"]
     pagination_class = SizePagePagination
 
@@ -33,6 +34,28 @@ class UserDetailSuspendView(generics.RetrieveDestroyAPIView):
     queryset = Account.objects.filter(status="active")
     serializer_class = AccountSerializer
 
+    def delete(self, request, *args, **kwargs):
+        instance = self.get_object()
+        instance.status = "suspended"
+        instance.save()
+        return Response(status=status.HTTP_204_NO_CONTENT)
+
+
+class UserDetailMerchantView(DetailMultipleFieldLookupMixin, generics.RetrieveDestroyAPIView):
+    # permission_classes = [IsAuthenticated]
+    queryset = Account.objects.filter(status="active")
+    serializer_class = AccountSerializer
+    lookup_query_fields = {
+        'merchant_code': 'merchant__code',
+        'user_id': 'user_id',
+    }
+    lookup_fields = ['merchant_code', 'user_id']
+
+    @swagger_auto_schema(operation_id="merchant_user_read")
+    def get(self, request, *args, **kwargs):
+        return super().get(request, *args, **kwargs)
+
+    @swagger_auto_schema(operation_id="merchant_user_delete")
     def delete(self, request, *args, **kwargs):
         instance = self.get_object()
         instance.status = "suspended"
